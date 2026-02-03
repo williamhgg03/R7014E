77a1c7bdb60fbb42c19572a3598222bdbbf26fa4
Majorsilver6969 <ammar.chams.al.deen@gmail.com>
2026-02-03 15:58:45 +0100

    yesyyes

--- LabA/TankSystem.m
+++ LabA/TankSystem.m
@@ -2,19 +2,22 @@
 At = 33;
 Ao = 0.16;
 g = 981;
-K = 5;
+K = 4;
 T = 35;
 h_0 = 3.2;
-
+df_u_0_At = 7.54; 
 % equilibrium point
 h_10 = 8; 
 h_20 = 8;
 
 
-
 A = [-Ao*g*(1/(At*sqrt(2*g*(h_10+h_0)))) 0 ; 
-    Ao*g*(1/(At*sqrt(2*g*(h_10+h_0)))) -Ao*g*(-1/(At*sqrt(2*g*(h_20+h_0))))];
-B = [6.95/36.2; 0];
+    Ao*g*(1/(At*sqrt(2*g*(h_10+h_0)))) Ao*g*(-1/(At*sqrt(2*g*(h_20+h_0))))];
+B = [df_u_0_At/At; 0];
 N = eye(2);
 C = [0 1];
 D = 0;
+
+sys = ss(A,B,C,D);
+
+
