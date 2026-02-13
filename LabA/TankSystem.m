defc5aa545590d0dd89c283b309f35a1f7956b0d
William Hägg <wilhgg-2@student.ltu.se>
2026-02-13 17:02:40 +0100

    lagt in estimators i interfacen till tank systemet

--- LabA/TankSystem.m
+++ LabA/TankSystem.m
@@ -14,8 +14,9 @@ dq_0 = 7.28; % 7.54
 u_0 = 1.39364;
 h_10 = 8;
 h_20 = 8;
+Al_0 = 0;
 x_hat_0 = [h_10; h_20];
-Al_0 = 0; 
+x_hat_0_dist = [h_10; h_20; Al_0];
 
 % System equation
 sqrt1 = sqrt(2*g*(h_10+h_0));
@@ -44,6 +45,9 @@ R = 0.2;
 
 % Some mesurements
 %u_measured = [0.4 0.8 1 1.1 1.2 1.3 1.9 2.5];
+
+% Is the sensor fucked? measurment from upper tank does not seem to reflect
+% reality...
 h_1_V = [0.54 0.965 1.34 1.72 2.01 2.43 2.86 3.215 3.62 4.01 4.39 4.76 5.17 5.51 5.91 6.28 6.66 7.05 7.49 7.83 8.2 8.5 9.3 9.6 9.95];
 h_1 =[2.5 3 3.5 4 4.5 5 5.5 6 6.5 7 7.5 8 8.5 9 9.5 10 10.5 11 11.5 12 12.5 13 13.5 14.5 15];
 
@@ -67,5 +71,5 @@ Dad = Da;
 % Kalman Filter gain with disturbance
 Q_dist = [Variance 0 0; 
           0 Variance 0;
-          0 0 1e-7];
+          0 0 1e-8];
 [K_dist, P_dist] = dlqe(Aad, eye(size(Aad)), Cad, Q_dist, R);
